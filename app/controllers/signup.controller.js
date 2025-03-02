b26c67e1a42f592c1604cd439d0d53627daafd78
Dawntless <66586346+Mowpey@users.noreply.github.com>
2025-03-02 18:13:12 +0800

    made uploading image work

--- app/controllers/signup.controller.js
+++ app/controllers/signup.controller.js
@@ -1,9 +1,35 @@
-import bcrypt from "bcrypt"
-import { PrismaClient } from "@prisma/client"
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const signUp = async (req, res) => {
+  if (req.userType == "jobSeeker") {
+    const user = await prisma.user.create({
+      data: {
+        firstName: req.body.firstName,
+        middleName: req.body.middleName,
+        lastName: req.body.lastName,
+        suffixName: req.body.suffixName,
+        gender: req.body.gender,
+        age: parseInt(req.body.age),
+        emailAddress: req.body.emailAddress,
+        password: bcrypt.hashSync(req.body.password, 10),
+        profileImage: req.file.filename,
+        bio: req.body.bio,
+        barangay: req.body.barangay,
+        street: req.body.street,
+        houseNumber: req.body.houseNumber,
+        userType: req.body.userType,
+        jobSeeker: {
+          availablity: req.body.availablity,
+          //credentials: req.body.credentials,
+          hourlyRate: req.body.hourlyRate,
+        },
+      },
+    });
+    res.json(user);
+  }
 
   const user = await prisma.user.create({
     data: {
@@ -14,15 +40,15 @@ const signUp = async (req, res) => {
       gender: req.body.gender,
       age: parseInt(req.body.age),
       emailAddress: req.body.emailAddress,
-      password: bcrypt.hashSync(req.body.password,10),
-      profileImage: req.body.profileImage,
+      password: bcrypt.hashSync(req.body.password, 10),
+      profileImage: req.file.filename,
       bio: req.body.bio,
       barangay: req.body.barangay,
       street: req.body.street,
       houseNumber: req.body.houseNumber,
-      userType: req.body.userType
-    }
-  })
-  res.json(user)
-}
-export default signUp
+      userType: req.body.userType,
+    },
+  });
+  res.json(user);
+};
+export default signUp;
--- app/routes/app.routes.js
+++ app/routes/app.routes.js
@@ -1,16 +1,30 @@
-import express from "express"
-import * as HomeController from "../controllers/app.controller.js"
-import signUp from "../controllers/signup.controller.js"
-import multer from "multer"
+import express from "express";
+import * as HomeController from "../controllers/app.controller.js";
+import signUp from "../controllers/signup.controller.js";
+import multer from "multer";
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    let today = new Date(Date.now());
+    let time = new Date(Date.now());
+    let splitFile = file.originalname.split(".");
 
-const router = express.Router()
-const formData = multer()
+    today = today.toLocaleDateString().replaceAll("/", "-");
+    time = time.getHours() + "-" + time.getMinutes() + "-" + time.getSeconds();
 
+    const fileOutput =
+      splitFile[0] + "-" + today + "-" + time + "." + splitFile[1];
+    cb(null, fileOutput);
+  },
+});
 
+const router = express.Router();
+const formData = multer({ storage });
 
-router.get("/", HomeController.getHome)
-router.post("/signup", formData.array(), signUp)
+router.get("/", HomeController.getHome);
+router.post("/signup", formData.single("profileImage"), signUp);
 
-
-export default router
+export default router;
