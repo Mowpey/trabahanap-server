0f4649eecc4dbc94a1c21c27302c7b40b821c27d
Dawntless <66586346+Mowpey@users.noreply.github.com>
2025-04-22 14:18:06 +0800

    fix for signing up

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import http from "http"
+import http from "http";
 import appRouter from "./routes/app.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
@@ -7,17 +7,25 @@ import { initializeSocketIO } from "./controllers/socket.io.controller.js";
 import communityRoutes from "./routes/community.routes.js";
 import cors from "cors";
 
-
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.static("../app"));
-app.use(express.json());
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const httpServer = http.createServer(app);
 const io = initializeSocketIO(httpServer);
-app.set('socketio',io);
-
+app.set("socketio", io);
 
 app.use(authRoutes);
 app.use(appRouter);
@@ -25,6 +33,6 @@ app.use(chatRoutes);
 app.use(communityRoutes);
 
 const PORT = 3000;
-httpServer.listen(PORT,() => {
-    console.log('Server running on port ', PORT)
-})
+httpServer.listen(PORT, () => {
+  console.log("Server running on port ", PORT);
+});
