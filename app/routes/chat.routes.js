184214d7ec8db33c848aa6ed9cb6cd69025549c3
Donkey <114471039+kentobi09@users.noreply.github.com>
2025-04-24 01:21:27 +0800

    view profile on chat

--- app/routes/chat.routes.js
+++ app/routes/chat.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createChat,getUserChats,sendMessage,getMessages,getStatus,chatReject,chatApprove,getReadStatus} from "../controllers/chat.controller.js";
+import { getReviews,createChat,getUserChats,sendMessage,getMessages,getStatus,chatReject,chatApprove,getReadStatus,getJobSeekerTags,getUserProfile} from "../controllers/chat.controller.js";
 import authenticateToken from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -11,6 +11,8 @@ router.get("/api/messages/:chatId",authenticateToken,getMessages);
 router.get('/chats/:chatId/status',authenticateToken,getStatus);
 router.post('/chats/:chatId/approve',authenticateToken,chatApprove);
 router.post('/chats/:chatId/reject',authenticateToken,chatReject);
-router.get('/api/message/read-status/:messageId',authenticateToken,getReadStatus)
-
+router.get('/api/message/read-status/:messageId',authenticateToken,getReadStatus);
+router.get('/api/job-seeker/:id/tags', authenticateToken, getJobSeekerTags);
+router.get('/user/profile/:id/details',authenticateToken ,getUserProfile);
+router.get('/user/reviews/:id',authenticateToken ,getReviews);
 export default router;
